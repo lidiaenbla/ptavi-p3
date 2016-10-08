0cf1a8ac12650df3f026758f2c2f9d78c4f29ee3
lidiaenbla <lidiaeb1993@gmail.com>
2016-10-08 11:51:25 +0200

    pasado el pep8, aun 4 errores

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,9 +5,10 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import sys
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
 
         self.width = ""
         self.heigth = ""
@@ -21,27 +22,26 @@ class SmallSMILHandler(ContentHandler):
         self.begin = ""
         self.dur = ""
         self.misdatos = []
-       
 
-    def startElement(self,etiqueta,attrs):
+    def startElement(self, etiqueta, attrs):
 
         if etiqueta == 'root-layout':
-            rootlayout = {'rootlayout' : ({'width' : attrs.get('width',""),'height' : attrs.get('height',""),'background-color': attrs.get('background-color')})}
+            rootlayout = {'rootlayout': ({'width': attrs.get('width', ""), 'height': attrs.get('height', ""), 'background-color': attrs.get('background-color')})}
             self.misdatos.append(rootlayout)
         elif etiqueta == 'region':
-            region = {'region' : ({'id' : attrs.get('id', ""), 'top' : attrs.get('top', ""), 'bottom' : attrs.get('bottom',""), 'left' : attrs.get('left', ""),'right' : attrs.get('right',"")})}
+            region = {'region': ({'id': attrs.get('id', ""), 'top': attrs.get('top', ""), 'bottom': attrs.get('bottom', ""), 'left': attrs.get('left', ""), 'right': attrs.get('right', "")})}
             self.misdatos.append(region)
         elif etiqueta == 'img':
-            img = {'img' : ({'src' : attrs.get('src', ""), 'region' : attrs.get('region', ""),'begin' : attrs.get('begin', ""), 'dur' : attrs.get('dur', "")})}
+            img = {'img': ({'src': attrs.get('src', ""), 'region': attrs.get('region', ""), 'begin': attrs.get('begin', ""), 'dur': attrs.get('dur', "")})}
             self.misdatos.append(img)
         elif etiqueta == 'audio':
-            audio = {'audio' : ({'src' : attrs.get('src', ""), 'abegin' : attrs.get('begin', ""), 'dur' : attrs.get('dur', "")})}
+            audio = {'audio': ({'src': attrs.get('src', ""), 'abegin': attrs.get('begin', ""), 'dur': attrs.get('dur', "")})}
             self.misdatos.append(audio)
         elif etiqueta == 'textstream':
-            textstream = {'textstream' : ({'src' : attrs.get('src',"")})}
+            textstream = {'textstream': ({'src': attrs.get('src', "")})}
             self.misdatos.append(textstream)
 
-    def get_tags (self):
+    def get_tags(self):
         return self.misdatos
 
 if __name__ == "__main__":
