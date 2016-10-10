36f95e4dd1e39b2d0adea44d7fd75132e28d8708
lidiaenbla <lidiaeb1993@gmail.com>
2016-10-10 19:48:40 +0200

    terminado ej6, aun sin pep8

--- karaoke.py
+++ karaoke.py
@@ -8,28 +8,47 @@ import json
 import urllib.request
 from smallsmilhandler import SmallSMILHandler
 
+class KaraokeLocal():
+    def Inicializador(self, fichero):
+        self.cHandler = SmallSMILHandler()
+        self.misdatos = []
+        self.fich = fichero
+
+        parser = make_parser()
+        parser.setContentHandler(self.cHandler)
+        parser.parse(open(self.fich))
+        misdatos = self.cHandler.get_tags()
+
+        return misdatos
+
+    def toStr(self, datos):
+        salida = ""
+
+        for elementos in datos:
+            for etiqueta in elementos:
+                for atributo, valor in elementos[etiqueta].items():
+                    if (atributo == 'src') and (valor[0:7] == 'http://'):
+                        valorN = valor.split("/")[-1]
+                        urllib.request.urlretrieve(valor, valorN)
+                        salida = salida + '\t' + atributo + '="' + valorN + '"\t'
+                    else:
+                        salida = salida + '\t' + atributo + ' = "' + valor + '"\t'
+            print(etiqueta + salida)
+            salida = ""
+
+    def to_json(self, datos):
+        json.dump(datos, open('karaoke.json', 'w'))
+
 if __name__ == "__main__":
 
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
+    objKaraokeLocal = KaraokeLocal()
+    datos = []
+
     try:
         fichero = sys.argv[1]
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
 
-    parser.parse(open(fichero))
-    misdatos = cHandler.get_tags()
-    salida = ""
-    for datos in misdatos:
-        for etiqueta in datos:
-            for atributo, valor in datos[etiqueta].items():
-                if (atributo == 'src') and (valor[0:7] == 'http://'):
-                    valorN = valor.split("/")[-1]
-                    urllib.request.urlretrieve(valor, valorN)
-                    salida = salida + '\t' + atributo + '="' + valorN + '"\t'
-                else:
-                    salida = salida + '\t' + atributo + ' = "' + valor + '"\t'
-        print(etiqueta + salida)
-        salida = ""
-    json.dump(misdatos, open('karaoke.json', 'w'))
+    datos = objKaraokeLocal.Inicializador(fichero)
+    objKaraokeLocal.toStr(datos)
+    objKaraokeLocal.to_json(datos)
