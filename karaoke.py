f6fa6e74b4a5610583fb31b7c3f1bd776ec675ab
lidiaenbla <lidiaeb1993@gmail.com>
2016-10-10 10:45:11 +0200

    acabado ej4

--- karaoke.py
+++ karaoke.py
@@ -4,23 +4,27 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import sys
+import json
 from smallsmilhandler import SmallSMILHandler
 
 if __name__ == "__main__":
 
-	parser = make_parser()
-	cHandler = SmallSMILHandler()
-	parser.setContentHandler(cHandler)
-	try:
-		fichero = sys.argv[1]
-	except IndexError:
-		sys.exit("Usage: python3 karaoke.py file.smil")
-	parser.parse(open(fichero))
-	misdatos = cHandler.get_tags()
-	salida = ""
-	for datos in misdatos:
-		for etiqueta in datos:
-			for atributo, valor in datos[etiqueta].items():
-				salida = salida + etiqueta + " \ " + atributo + '="' + valor + '"\ '
-		print(salida)
-		salida = ""
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    try:
+        fichero = sys.argv[1]
+    except IndexError:
+        sys.exit("Usage: python3 karaoke.py file.smil")
+
+    parser.parse(open(fichero))
+    misdatos = cHandler.get_tags()
+    salida = ""
+    for datos in misdatos:
+        for etiqueta in datos:
+            for atributo, valor in datos[etiqueta].items():
+                salida = salida + '\t' + atributo + ' = "' + valor + '"\t'
+        print(etiqueta + salida)
+        salida = ""
+    json.dump(misdatos, open('karaoke.json', 'w'))
+
